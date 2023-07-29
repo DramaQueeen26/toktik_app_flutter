8e972270ad02ea3f35f48c3fa7b66d16086af599
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-07-29 10:41:24 -0400

    Estructura inicial y agregando provider

---
+++ lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:toktik_app_flutter/domain/entities/video_post.dart';
+
+class DiscoverProvider extends ChangeNotifier{
+
+  bool initialLoading = true;
+  List<VideoPost> videos = [];
+
+  Future<void> loadNextPage() async {
+
+    notifyListeners();
+
+  }
+
+}
