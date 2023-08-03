03820dc13fc3b51fcc250b8e80f5a64b022a8bfd
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-08-03 10:18:02 -0400

    CircularProgress y PageView

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:toktik_app_flutter/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  
+  final List<VideoPost> videos;
+  
+  const VideoScrollableView({
+    super.key, 
+    required this.videos
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+        Container(color: Colors.teal),
+        Container(color: Colors.yellow),
+        Container(color: Colors.purple),
+      ],
+    );
+  }
+}
