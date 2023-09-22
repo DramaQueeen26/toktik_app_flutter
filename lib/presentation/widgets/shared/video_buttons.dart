6207335946d09a5e63beff513fec628e05d90aec
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-22 17:26:03 -0400

    Icono girando - animate_do

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik_app_flutter/domain/entities/video_post.dart';
 
@@ -17,7 +18,14 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
-        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined)
+        const SizedBox( height: 20 ),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox( height: 20 ),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration( seconds: 5 ),
+          child: const _CustomIconButton(value: 0, iconData: Icons.play_circle_outlined)
+        )
       ],
     );
   }
@@ -43,6 +51,7 @@ class _CustomIconButton extends StatelessWidget {
           onPressed: () {}, 
           icon: Icon( iconData, color: color, size: 30)
         ),
+        if ( value > 0 )
         Text(NumberFormats.numberFormat( value.toDouble() ))
       ],
     );
